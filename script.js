999f608010c67b0cba82b94a3c7b1cc01ff5743a
Gustavo <gustavo-olimpio@hotmail.com>
2023-01-06 13:14:59 -0300

    javascript checkmark pronto

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-function seleciona(classe,icone){
-    const variavel = document.querySelector(".selecionado")
-    const icon = document.querySelector(".aparece")
+function seleciona(classe,icone,lugar){
+    const tudo = document.querySelector(lugar)
+    console.log(tudo)
+    const variavel = tudo.querySelector(".selecionado")
+    const icon = tudo.querySelector(".aparece")
     console.log(icon);
+    console.log(variavel);
     if (variavel !== null){
         variavel.classList.remove("selecionado")
         icon.classList.remove("aparece")
     }
-    const newicon = document.querySelector(icone);
-    const valor = document.querySelector(classe);
+    const newicon = tudo.querySelector(icone);
+    const valor = tudo.querySelector(classe);
+    console.log(newicon);
+    console.log(valor);
     valor.classList.add("selecionado")
     newicon.classList.add("aparece")
     
