c463220c6d7cbb4988e632168a6b6c57f01682e7
Gustavo <gustavo-olimpio@hotmail.com>
2023-01-06 14:41:08 -0300

    Botaoverde pronto

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 function seleciona(classe,icone,lugar){
     const tudo = document.querySelector(lugar)
-    console.log(tudo)
+    
     const variavel = tudo.querySelector(".selecionado")
     const icon = tudo.querySelector(".aparece")
-    console.log(icon);
-    console.log(variavel);
+    
     if (variavel !== null){
         variavel.classList.remove("selecionado")
         icon.classList.remove("aparece")
     }
     const newicon = tudo.querySelector(icone);
     const valor = tudo.querySelector(classe);
-    console.log(newicon);
-    console.log(valor);
+
     valor.classList.add("selecionado")
     newicon.classList.add("aparece")
-    
-    
+    botaoverde()
+}
+
+function botaoverde(){
+    const principal = document.querySelector('.principal .selecionado')
+    const bebida = document.querySelector('.bebida .selecionado')
+    const sobremesa = document.querySelector('.sobremesa .selecionado')
+    console.log(principal);
+    console.log(bebida);
+    console.log(sobremesa);
+    if (principal !== null & bebida !== null & sobremesa !== null){
+        const botao = document.querySelector('.button')
+        botao.classList.add('verde')
+        document.querySelector('.txtbotao').innerHTML='Fechar pedido'
+    }
 }
